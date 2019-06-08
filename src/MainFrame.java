b5c635eceb24b0aff015d85b3ecd1524cca23011
buczu1612 <pjbuczynski@gmail.com>
2019-06-08 15:52:46 +0200

    First Changes

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,10 +1,54 @@
+import java.awt.BorderLayout;
+import java.awt.LayoutManager;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class MainFrame extends JFrame {
-
+	
+	JButton runButton;
+	JPanel leftPanel;
+	JPanel rightPanel;
+	JLabel leftMatrix;
+	JLabel rightMatrix;
+	
+	
+	
+	
+	public MainFrame() {
+		setSize(800,600);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Fork-Join Framework Example");
+		
+		
+		leftPanel = new JPanel();
+		add(leftPanel, BorderLayout.LINE_START);
+		rightPanel = new JPanel();
+		add(rightPanel, BorderLayout.LINE_END);
+		
+		runButton = new JButton("Run");
+		leftPanel.add(runButton, BorderLayout.PAGE_END);
+		
+		
+		
+		
+		
+		setVisible(true);
+	
+	}
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		MainFrame frame = new MainFrame();
+		
 	}
 
 }
