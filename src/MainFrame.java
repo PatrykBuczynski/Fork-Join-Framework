38bc4ab41a1ad4f3e0b59580835dbf17e20bc6ca
buczu1612 <pjbuczynski@gmail.com>
2019-06-08 17:11:39 +0200

    Basic Gui

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,18 +1,29 @@
 import java.awt.BorderLayout;
-import java.awt.LayoutManager;
+import java.awt.Color;
+import java.awt.GridLayout;
 
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
 
 public class MainFrame extends JFrame {
 	
 	JButton runButton;
+	
 	JPanel leftPanel;
 	JPanel rightPanel;
-	JLabel leftMatrix;
-	JLabel rightMatrix;
+	
+	JTextArea leftMatrix;
+	JTextArea rightMatrix;
+	
+	JLabel leftTitle;
+	JLabel rightTitle;
+	JLabel multiplyInfo;
 	
 	
 	
@@ -21,17 +32,49 @@ public class MainFrame extends JFrame {
 		setSize(800,600);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Fork-Join Framework Example");
+		this.setLayout(new GridLayout(1,2));
+		
 		
 		
 		leftPanel = new JPanel();
-		add(leftPanel, BorderLayout.LINE_START);
+		leftPanel.setLayout(new GridLayout(3,1));
+		leftPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		add(leftPanel);
+		
 		rightPanel = new JPanel();
-		add(rightPanel, BorderLayout.LINE_END);
+		rightPanel.setLayout(new GridLayout(3,1));
+		rightPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		add(rightPanel);
 		
+
+		String inicialArea = new String();
+		/*Wypelniamy macierz 10x10 do wyswietlenia wypelniona zerami */
+		for(int i = 0; i < 5; i++) {  					
+			for(int j = 0; j < 5; j++) {
+
+				inicialArea =  inicialArea.concat("| 0 ");
+				
+				
+			}
+			inicialArea = inicialArea.concat(" |\n");
+		}
+
+		leftTitle = new JLabel("Input Matrices");
+		leftPanel.add(leftTitle);
+		leftMatrix = new JTextArea(inicialArea);
+		leftMatrix.setLineWrap(true);
+		leftPanel.add(leftMatrix);
 		runButton = new JButton("Run");
-		leftPanel.add(runButton, BorderLayout.PAGE_END);
+		leftPanel.add(runButton);
 		
-		
+		rightTitle = new JLabel("Output Matrices");
+		rightPanel.add(rightTitle);
+		rightMatrix = new JTextArea(inicialArea);
+		rightMatrix.setLineWrap(true);
+		rightPanel.add(rightMatrix);
+		multiplyInfo = new JLabel("Na razie nic tu nie ma"); // Prawdopodobnie trzeba to bedzie zmienic - obecnie robi jako placeholder
+		rightPanel.add(multiplyInfo);
+
 		
 		
 		
