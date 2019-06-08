7567c68e389ae814f3a5405cfb4c1823674c1139
buczu1612 <pjbuczynski@gmail.com>
2019-06-08 19:07:32 +0200

    Poprawione Labele

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -10,6 +10,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 
 
 public class MainFrame extends JFrame {
@@ -35,31 +36,48 @@ public class MainFrame extends JFrame {
 		setTitle("Fork-Join Framework Example");
 		this.setLayout(new GridLayout(1,2));
 		
+
 		leftPanel = new JPanel(new BorderLayout());
+
+		leftPanel = new JPanel(new BorderLayout());
+
 		leftPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		add(leftPanel);
 		
+
 		rightPanel = new JPanel(new BorderLayout());
+
+		rightPanel = new JPanel(new BorderLayout());
+
 		rightPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		add(rightPanel);
 		
 
 
-		leftTitle = new JLabel("Input Matrices");
+		leftTitle = new JLabel("Input Matrices", SwingConstants.CENTER);
 		leftPanel.add(leftTitle, BorderLayout.NORTH);
 		leftPanel.add(new Matrix(true).scroll, BorderLayout.CENTER);
 		runButton = new JButton("Run");
+
 		leftPanel.add(runButton, BorderLayout.SOUTH);
 		leftSlider = new JSlider(JSlider.VERTICAL, 0, 99, 0);
 		leftPanel.add(leftSlider, BorderLayout.WEST);
+
+		leftPanel.add(runButton, BorderLayout.SOUTH);
+		leftSlider = new JSlider(JSlider.VERTICAL, 0, 99, 0);
+		leftPanel.add(leftSlider, BorderLayout.WEST);
+
 		
-		rightTitle = new JLabel("Output Matrices");
+		rightTitle = new JLabel("Output Matrices", SwingConstants.CENTER);
 		rightPanel.add(rightTitle, BorderLayout.NORTH);
 		rightPanel.add(new Matrix(false).scroll, BorderLayout.CENTER);
 		multiplyInfo = new JLabel("Na razie nic tu nie ma"); // Prawdopodobnie trzeba to bedzie zmienic - obecnie robi 
 		rightPanel.add(multiplyInfo, BorderLayout.SOUTH);
 		rightSlider = new JSlider(JSlider.VERTICAL, 0, 99, 0);
+
 		rightPanel.add(rightSlider, BorderLayout.WEST);
+		rightPanel.add(rightSlider, BorderLayout.WEST);
+
 		
 		
 		
